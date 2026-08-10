65f209c6ef7ead3ef255904719b84762cc57ae63
rahul-tandel <rahult.blackcurrant@gmail.com>
2026-08-10 12:10:58 +0530

    asdasd

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [new URL("https://images.unsplash.com/photo-1786108503407-69fd0bda97b5?q=80&w=1468&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")],
+  }
 };
 
 export default nextConfig;
